81992c6421b02728b1a472d451a36b2c65785398
andrii-svyshch <80419679+andrii-svyshch@users.noreply.github.com>
2021-11-13 11:51:39 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
+
+class App extends Component {
+  static defaultProps = {};
+
+  static propTypes = {};
+
+  state = {};
+
+  render() {
+    return <div className="container"></div>;
+  }
+}
+
+export default App;
